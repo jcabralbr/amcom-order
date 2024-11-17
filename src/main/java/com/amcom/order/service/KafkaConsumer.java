808007ccaa9acd0e5c2070b7bbf422a1d648516f
Julio Cesar Cabral <julio.cabral@brmalls.com.br>
2024-11-16 23:52:28 -0300

    atualizando

--- src/main/java/com/amcom/order/service/KafkaConsumer.java
+++ src/main/java/com/amcom/order/service/KafkaConsumer.java
@@ -2,10 +2,12 @@ package com.amcom.order.service;
 
 import com.amcom.order.dto.OrderDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class KafkaConsumer {
 
@@ -13,6 +15,7 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "purchase_order", groupId = "order-group")
     public void consumeOrder(OrderDTO orderDTO) {
+        log.info("recebendo o pedido {}, da fila purchase_order, para processamento", orderDTO.orderId());
         orderService.processOrder(orderDTO);
     }
 }
